656b61899e0831912a84b596fa2aa79d839d6327
Arthur Gautheron <gauthero@etud.insa-toulouse.fr>
2023-03-14 14:14:04 +0100

    choosing attack 4 dataleak

--- zephyr-docker/workspace/ripe/src/ripe_attack_generator.c
+++ zephyr-docker/workspace/ripe/src/ripe_attack_generator.c
@@ -124,7 +124,7 @@ main(void)
   __asm__(".insn u 0x7B, x0, 0" : : : ); 
   __asm__(".insn u 0x0B, x5, 831" : : : ); 
   __asm__(".insn u 0x2B, x6, 91" : : : ); 
-#define ATTACK_NR   2
+#define ATTACK_NR   4
 // 1-5-9 ok  8 ok
 #if ATTACK_NR == 1  // patch retaddr
     attack.technique = DIRECT;
