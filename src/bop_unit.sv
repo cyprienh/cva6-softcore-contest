0c01d813130e0ab1d3da5e92367c16492dc20974
holmesemily <holmesemily82@gmail.com>
2023-04-16 22:56:57 +0200

    new bop_unit with memset commented, perf_baseline crash, creates intervals

--- src/bop_unit.sv
+++ src/bop_unit.sv
@@ -22,7 +22,9 @@ module bop_unit (
     output logic [31:0]                           alu_read_out2,
 
     output logic       to_crash,
+    output logic       illegal_load,
     output logic       data_in_buffer,
+    output logic [6:0] load_reg,
 
     input logic       rst_buf_i,
     input logic       en_crash_i
@@ -32,37 +34,40 @@ module bop_unit (
     riscv::xlen_t             vaddr_xlen;
     
     // INSA: Registers for overflow management (heap)
-    parameter   bof_write_size = 32;
+    parameter   bof_write_size = 32;    // 200 -> crash -- 1000 -> pas crash -> 416 (5*message ?)
     parameter   bof_date_max = 10;
-    parameter   bof_load_time = 2;
 
     logic[31:0] bof_start_d;
     logic[31:0] bof_end_d;
     logic       bof_active_d;
     logic       bof_load_in_range_d;
     logic[31:0] bof_count_d;
+    logic[31:0] bof_same_d;
     logic[3:0]  bof_date_d;
-    logic[4:0]  bof_load_limit_d;
 
     logic[31:0] bof_start_q;
     logic[31:0] bof_end_q;
     logic       bof_active_q;
     logic       bof_load_in_range_q;
     logic[31:0] bof_count_q;
+    logic[31:0] bof_same_q;
     logic[3:0]  bof_date_q;
-    logic[4:0]  bof_load_limit_q;
 
     logic[31:0] bof_pc_d;
     logic[31:0] bof_pc_q;
-
-    logic[31:0]  bof_store_size;
     
     logic       buffer_write_d;
     logic       buffer_write_q;
     logic       addr_in_buffer;
 
-    logic en_crash_q;
-    logic en_crash_d;
+    logic[6:0] bof_last_reg_d;
+    logic[6:0] bof_last_reg_q;
+
+    logic[31:0]  bof_last_data_d;
+    logic[31:0]  bof_last_data_q;
+
+    logic       illegal_load_d;
+    logic       illegal_load_q;
 
     assign vaddr_xlen = $unsigned($signed(fu_data_i.imm) + $signed(fu_data_i.operand_a));
     assign vaddr_i = vaddr_xlen[riscv::VLEN-1:0];
@@ -83,15 +88,8 @@ module bop_unit (
 
     assign data_in_buffer = bof_active_q; //debug
     assign to_crash = bof_load_in_range_q;
-
-    always_comb begin : store_size
-      case(decoded_instr_i.op)
-        ariane_pkg::SW: bof_store_size = 4;
-        ariane_pkg::SH: bof_store_size = 2;
-        ariane_pkg::SB: bof_store_size = 1;
-        default:        bof_store_size = 0;
-      endcase
-    end
+    assign load_reg = bof_last_reg_q;
+    assign illegal_load = illegal_load_q;
 
     always_comb begin : heap_safe
       buffer_write_d = 1'b0;
@@ -102,36 +100,61 @@ module bop_unit (
       bof_load_in_range_d = bof_load_in_range_q;
       bof_count_d = bof_count_q;
       bof_date_d = bof_date_q;
-      en_crash_d = en_crash_q;
+      bof_same_d = bof_same_q;
       
       bof_pc_d = bof_pc_q;
-      bof_load_limit_d = bof_load_limit_q;
+      bof_last_data_d = bof_last_data_q;
 
-      if(decoded_instr_i.op == ariane_pkg::LW) begin   // if load inside one overflow range, take note 
-        if (addr_in_buffer) begin           // test to fix lw/jalr visiblement ça fait rien
+      bof_last_reg_d = bof_last_reg_q;
+      illegal_load_d = illegal_load_q;
+
+      // DETECTING INTERACTIONS WITH SAVED INTERVALS
+      if(decoded_instr_i.op == ariane_pkg::LW && !(decoded_instr_i.rs1 inside {2, 8})) begin   // if load inside one overflow range, take note 
+        if (addr_in_buffer) begin
           bof_load_in_range_d = 1'b1;
+          bof_last_reg_d = decoded_instr_i.rd;
+        end else if (decoded_instr_i.rs1 == bof_last_reg_q && bof_load_in_range_q) begin // if illegal load in a row
+          illegal_load_d = 1'b1;
         end else begin
           bof_load_in_range_d = 1'b0;
+          bof_last_reg_d = 0;
         end
       end
+       if(decoded_instr_i.op == ariane_pkg::LW && !(decoded_instr_i.rs1 inside {2, 8})) begin   // if load inside one overflow range, take note 
+         if (addr_in_buffer) begin
+           bof_load_in_range_d = 1'b1;
+           bof_last_reg_d = decoded_instr_i.rd;
+         end else if (decoded_instr_i.rs1 == bof_last_reg_q && bof_load_in_range_q) begin // if illegal load in a row
+           illegal_load_d = 1'b1;
+         end else begin
+           bof_load_in_range_d = 1'b0;
+           bof_last_reg_d = 0;
+         end
+       end
 
-      if ((bof_pc_q != decoded_instr_i.pc) && en_crash_i) begin 
+      // SAVING INTERVALS
+      if (bof_pc_q != decoded_instr_i.pc && en_crash_i) begin 
         bof_pc_d = decoded_instr_i.pc;
-        if(decoded_instr_i.op == ariane_pkg::SB && vaddr_i[31:28] == 4'h8) begin
+        if(decoded_instr_i.op == ariane_pkg::SB) begin
           if(!(decoded_instr_i.rs1 inside {2, 8})) begin
+            bof_last_data_d = fu_data_i.operand_b;
             if(~bof_active_q) begin     // start tracking
               bof_active_d = 1'b1;
               bof_start_d = vaddr_i;
               bof_end_d = vaddr_i;
               bof_date_d = bof_date_max;
-              bof_count_d = 32'b0;
-            end else if(bof_end_q + bof_store_size == vaddr_i) begin    // if next store is next to previous one
+              bof_count_d = 32'b1;
+              bof_same_d = 32'b1;
+            end else if(bof_end_q + 1 == vaddr_i) begin    // if store is next to previous one
               bof_end_d = vaddr_i;
-              bof_count_d = bof_count_q + bof_store_size;
-              bof_date_d = bof_date_max;
+              bof_count_d = bof_count_q + 1;
+              bof_date_d = bof_date_max;     
+              if(fu_data_i.operand_b == bof_last_data_q) begin 
+                bof_same_d = bof_same_q + 1; 
+              end
             end else begin    // store somewhere new -> add to buffer
               bof_active_d = 1'b0;
-              if((bof_count_q > bof_write_size)) //test  
+              if(bof_count_q > bof_write_size)// && bof_same_q < bof_count_q) //test  
                 buffer_write_d = 1'b1;
             end
           end
@@ -141,18 +164,10 @@ module bop_unit (
               bof_date_d = bof_date_q - 1;
             else begin              // if date = 0, overflow timed out, writing
               bof_active_d = 1'b0;
-              if((bof_count_q > bof_write_size)) //test WOOOHOOO IT WORKS 
+              if(bof_count_q > bof_write_size) //&& bof_same_q < bof_count_q)
                 buffer_write_d = 1'b1;
             end
           end
-          
-            // load dans la range = (on était dans la range recemment || on est dans la range mtn)
-            // si on était déjà dans la range: range =-1 
-            // sinon (on load a l'instant): mettre la range au max
-          // end else if(decoded_instr_i.op == ariane_pkg::JALR) begin  // if call after lw in range, crash
-          //   if(bof_load_in_range_q && en_crash_i)
-          //     to_crash = 1'b1;
-          // end
         end
       end 
     end
@@ -168,6 +183,10 @@ module bop_unit (
         bof_date_q <= 4'b0;
         buffer_write_q <= 1'b0;
         bof_pc_q <= 32'b0;
+        bof_last_reg_q <= 6'b0;
+        illegal_load_q <= 1'b0;
+        bof_last_data_q <= 32'b0;
+        bof_same_q <= 32'b0;
       end else begin
         bof_active_q <= bof_active_d;
         bof_start_q <= bof_start_d;
@@ -177,6 +196,10 @@ module bop_unit (
         bof_date_q <= bof_date_d;
         buffer_write_q <= buffer_write_d;
         bof_pc_q <= bof_pc_d;
+        bof_last_reg_q <= bof_last_reg_d;
+        illegal_load_q <= illegal_load_d;
+        bof_last_data_q <= bof_last_data_d;
+        bof_same_q <= bof_same_d;
       end
     end
 
